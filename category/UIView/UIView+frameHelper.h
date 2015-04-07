4198894986ac180b456299ee0b890769d7c8aca5
liu poolo <liuyuanbolyb@126.come>
2015-04-07 17:21:56 +0800

    no message

--- category/UIView/UIView+frameHelper.h
+++ category/UIView/UIView+frameHelper.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (frameHelper)
-
+@property (nonatomic, assign)CGFloat x;
+@property (nonatomic, assign)CGFloat y;
+@property (nonatomic, assign)CGFloat centerX;
+@property (nonatomic, assign)CGFloat centerY;
+@property (nonatomic, assign)CGFloat width;
+@property (nonatomic, assign)CGFloat height;
 @end
