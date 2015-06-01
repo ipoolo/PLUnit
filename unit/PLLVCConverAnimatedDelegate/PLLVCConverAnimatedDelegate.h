39a36a7a8f7b6df81f82d87d97db548a44be6fcd
liu poolo <liuyuanbolyb@126.come>
2015-06-02 02:18:30 +0800

    fix 'bug'

--- unit/PLLVCConverAnimatedDelegate/PLLVCConverAnimatedDelegate.h
+++ unit/PLLVCConverAnimatedDelegate/PLLVCConverAnimatedDelegate.h
@@ -12,6 +12,8 @@
 
 @required
 -(UIView *)converView;
+-(BOOL)isConverViewTreeConstraintsWithTopLayoutGuide;
+//需要返回 返回的converView是否与 TopLayoutGuide 有约束 间接的也算(只要TopLayoutGuide 变化converView也要变化就算)
 
 @end
 
@@ -20,5 +22,7 @@
 @property (nonatomic,assign) BOOL transitionInteracted;
 @property (nonatomic,assign) CGFloat animationDuration;
 @property (nonatomic,assign) BOOL isPresenting;
+@property (nonatomic,assign) CGFloat percentToPop;
+@property (nonatomic,assign) CGFloat percentSpring;
 
 @end
--- unit/PLLVCConverAnimatedDelegate/PLLVCConverAnimatedDelegate.m
+++ unit/PLLVCConverAnimatedDelegate/PLLVCConverAnimatedDelegate.m
@@ -14,6 +14,8 @@
 
 @property (nonatomic,assign) CGRect converViewFromFrame;
 @property (nonatomic,assign) CGRect converViewToFrame;
+@property (nonatomic,assign) BOOL isDelegateForNavigationPush;
+@property (nonatomic,assign) BOOL isDelegateForNavigation;
 
 @end
 
@@ -31,7 +33,7 @@
 - (void)handlerPan:(UIPanGestureRecognizer *)pan{
     
     CGPoint translation = [pan translationInView:pan.view];
-    CGFloat percent = (translation.y/pan.view.bounds.size.height)*1.5;
+    CGFloat percent = (translation.y/pan.view.bounds.size.height)*(self.percentSpring?self.percentSpring:1.5);
     percent = fminf(fmaxf(percent, 0),1);
     
     
@@ -43,17 +45,46 @@
             
             break;
         case UIGestureRecognizerStateChanged:
-            [self updateInteractiveTransition:percent];
+            
+            if(percent<=1){
+                [self updateInteractiveTransition:percent];
+            }
+            break;
+        case UIGestureRecognizerStateCancelled:
+        case UIGestureRecognizerStateEnded:
+            if(percent>(self.percentToPop?self.percentToPop:0.4)){
+                [self finishInteractiveTransition];
+            }else{
+                [self cancelInteractiveTransition];
+            }
+            self.transitionInteracted = NO;
             break;
         default:
-            [self finishInteractiveTransition];
-            self.transitionInteracted = NO;
+            
+            break;
+            
     }
 }
 
 - (NSTimeInterval)transitionDuration:(id <UIViewControllerContextTransitioning>)transitionContext{
     
     return self.animationDuration<=0?2.0f:self.animationDuration;
+}
+
+-(UIView *)viewGetTopSuperView:(UIView *)view{
+    if(view.superview){
+        return [self viewGetTopSuperView:view.superview];
+    }else{
+        return view;
+    }
+}
+
+-(CGFloat)totalScorllViewOffset:(UIView *)view{
+    if(view.superview){
+        return [self totalScorllViewOffset:view.superview]+([view isKindOfClass:[UIScrollView class]]?((UIScrollView *)view).contentOffset.y:0);
+    }else{
+        return 0;
+    }
 }
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext{
@@ -69,23 +100,25 @@
     
     if([fromVC respondsToSelector:@selector(converView)]){
         fromConverView = [fromVC converView];
+        [fromConverView.superview bringSubviewToFront:fromConverView];
     }else{
         [NSException exceptionWithName:@"CustomError_fromVC_ConverView_invalid" reason:[NSString stringWithFormat:@"%@-fromVC_can't_respondsToSelector_converView",fromVC] userInfo:nil];
     }
     if([toVC respondsToSelector:@selector(converView)]){
         toConverView = [toVC converView];
+        [toConverView.superview bringSubviewToFront:toConverView];
     }else{
         [NSException exceptionWithName:@"CustomError_toVC_ConverView_invalid" reason:[NSString stringWithFormat:@"%@-toVC_can't_respondsToSelector_converView",toVC] userInfo:nil];
     }
     //其实只有 能responds才能执行过来...
-    
     
     [fromView setNeedsLayout];
     [fromView layoutIfNeeded];
     [toView setNeedsLayout];
     [toView layoutIfNeeded];
     
-    UIImageView *intermediaryFromView = [[UIImageView alloc] initWithImage:[fromConverView convertViewToImage]];
+    //   TODO:下一行test//
+    UIImageView *intermediaryView = [[UIImageView alloc] initWithImage:[fromConverView convertViewToImage]];
     
     
     CGFloat dissmissAlpha = 0.1f;
@@ -93,56 +126,85 @@
     
     //显示时获取位置 -只去一次 所以这块 from to 是相对 展示那次的
     if(self.isPresenting){
-        
         self.converViewFromFrame = [fromConverView.superview convertRect:fromConverView.frame toView:nil];
-        self.converViewToFrame = [toConverView.superview convertRect:toConverView.frame toView:nil];
+        self.converViewToFrame = [toConverView.superview convertRect:toConverView.frame toView:[self viewGetTopSuperView:toConverView]];
+        
+        if(self.isDelegateForNavigationPush){
+            //在最初计算conver的时候 push情况下 toView是没有navigationBar的(UILayoutGuide_top 在界面未移动到y<64(naivgatioNBar高度)之前 是=0的) 所以在ToView还未显示到界面y=0之前计算的conver 是少了这64高度的计算的 所以得减去这64的高度（少算了））
+            if([toVC respondsToSelector:@selector(isConverViewTreeConstraintsWithTopLayoutGuide)]&&[toVC isConverViewTreeConstraintsWithTopLayoutGuide]){
+                self.converViewToFrame = CGRectOffset(self.converViewToFrame, 0, 64);
+            }
+            
+        }
+        
     }
     
     if(self.isPresenting){
         [toView setTransform:offScreenBottom];
         [containerView addSubview:fromView];
         [containerView addSubview:toView];
-        [containerView addSubview:intermediaryFromView];
+        [containerView addSubview:intermediaryView];
         
     }else{
         toView.alpha = dissmissAlpha;
         [containerView addSubview:toView];
         [containerView addSubview:fromView];
-        [containerView addSubview:intermediaryFromView];
+        [containerView addSubview:intermediaryView];
     }
     
     CGRect toConverViewFrame = toConverView.frame;
-    toConverView.frame = [intermediaryFromView.superview convertRect:fromConverView.frame toView:toView];
+    intermediaryView.frame = self.isPresenting ? self.converViewFromFrame : self.converViewToFrame;
+    toConverView.frame = [intermediaryView.superview convertRect:intermediaryView.frame toView:toView];
     
-    intermediaryFromView.frame = self.isPresenting ? self.converViewFromFrame : self.converViewToFrame;
     
-    //转场动画中用中介view 来展示 fromView和toView的都隐藏
+    if(self.isDelegateForNavigation){
+        
+        toConverView.frame = CGRectOffset(toConverView.frame, 0, [self totalScorllViewOffset:toConverView]);
+        if([toVC respondsToSelector:@selector(isConverViewTreeConstraintsWithTopLayoutGuide)]&&[toVC isConverViewTreeConstraintsWithTopLayoutGuide]){
+            toConverView.frame = CGRectOffset(toConverView.frame, 0, -64);
+        }
+        
+        //在最初计算conver的时候 toView是没有navigationBar的(UILayoutGuide_top 在界面未移动到y<64(naivgatioNBar高度)之前 是=0的) 所以在ToView还未显示到界面y=0之前计算的conver 是少了这64高度的计算的 所以得减去这64的高度（少算了））
+        //因为有可能toView位于scrollView之上 因为我们是需要将view转换到 toConverView的坐标系上 那么当外部坐标转换到scrollView 内的时候 需要+ contentoffset.y 用于计算
+    }
+    
+    
+    //转场动画中用中介view 来展示 fromView
+    
     fromConverView.alpha = 0.0f;
-    
-    intermediaryFromView.alpha = 1.0f;
+    intermediaryView.alpha = 1.0f;
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
         if(self.isPresenting){
             fromView.alpha = dissmissAlpha;
             [toView setTransform:CGAffineTransformIdentity];
-            [intermediaryFromView setFrame:self.converViewToFrame];
+            [intermediaryView setFrame:self.converViewToFrame];
         }else{
             toView.alpha = 1.0f;
-            [intermediaryFromView setFrame:self.converViewFromFrame];
+            [intermediaryView setFrame:self.converViewFromFrame];
             [fromView setTransform:offScreenBottom];
         }
-        intermediaryFromView.alpha = 0.0f;
+        intermediaryView.alpha = dissmissAlpha;
         toConverView.frame = toConverViewFrame;
+        
     } completion:^(BOOL finished) {
         
-        [intermediaryFromView removeFromSuperview];
+        [intermediaryView removeFromSuperview];
         [toConverView setAlpha:1.0f];
         [fromConverView setAlpha:1.0f];
         toView.alpha = 1.0f;
-        fromView.alpha = 1.0;
+        fromView.alpha = 1.0f;
         [toConverView setNeedsLayout];
         [toConverView layoutIfNeeded];
-        [transitionContext completeTransition:YES];
+        if(transitionContext.transitionWasCancelled){
+            //cancel
+            //将改变的设置回初值位置
+            //fromView没改变(因为用了intermediaryView)
+            [toView setTransform:CGAffineTransformIdentity];
+            toConverView.frame = toConverViewFrame;
+            
+        }
+        [transitionContext completeTransition:!transitionContext.transitionWasCancelled];
         
     }];
 }
@@ -151,6 +213,8 @@
     
     if([presented respondsToSelector:@selector(converView)]&&[presenting respondsToSelector:@selector(converView)]){
         self.isPresenting = YES;
+        self.isDelegateForNavigation = NO;
+        self.isDelegateForNavigationPush = NO;
         return self;
     }else{
         return nil;
@@ -162,6 +226,8 @@
     
     if([dismissed respondsToSelector:@selector(converView)]){
         self.isPresenting = NO;
+        self.isDelegateForNavigation = NO;
+        self.isDelegateForNavigationPush = NO;
         return self;
     }else{
         return nil;
@@ -180,6 +246,12 @@
     
     if([fromVC respondsToSelector:@selector(converView)]&&[toVC respondsToSelector:@selector(converView)]){
         //navigation模式不同全部都是        self.isPresenting = YES;
+        if(operation==UINavigationControllerOperationPush){
+            self.isDelegateForNavigationPush = YES;
+        }else{
+            self.isDelegateForNavigationPush = NO;
+        }
+        self.isDelegateForNavigation = YES;
         self.isPresenting = YES;
         return self;
     }else{
